250142f5f7143ef544f035b28fe5e0619de2ba8e
Camilo Tobar <jc@swiset.com>
2020-12-02 08:25:21 -0500

    Second commit

--- src/com/icesi/Main.java
+++ src/com/icesi/Main.java
@@ -1,8 +1,23 @@
 package com.icesi;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    static BufferedReader reader;
+    static BufferedWriter writer;
+
+    public static void main(String[] args) throws IOException {
+        writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = reader.readLine();
+        while(line != "finish")
+        {
+
+        }
+
+        reader.close();
+        writer.close();
     }
 }
